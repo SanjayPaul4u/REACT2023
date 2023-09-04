81cbb6b91c5b88331ac469d663cc7d151daf01f3
SanjayPaul4u <sanjoypaul655@gmail.com>
2023-09-04 15:53:42 +0530

    VID-6: Understanding Props and propTypes

---
+++ src/module1.mjs
@@ -0,0 +1,7 @@
+// import second from './module2.mjs';
+// import {a} from './module2.mjs';
+import {a,b,c} from './module2.mjs';
+
+console.log(a);
+console.log(b);
+console.log(c); 
---
+++ src/module2.mjs
@@ -0,0 +1,8 @@
+const a = "Sanjay";
+const b = "Priyanka";
+const c = "Harry";
+
+// export default a;
+export {a};
+export {b};
+export {c};
